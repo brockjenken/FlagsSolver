83a3b85bbc5687ef12c0f3569ea04f16edae0c7a
brockjenken <brock.jenken@plusgrade.com>
2021-03-10 01:44:46 -0500

    Made a change

--- FlagsSolver/Service/BoardSolver.cs
+++ FlagsSolver/Service/BoardSolver.cs
@@ -17,7 +17,7 @@ namespace FlagsSolver.Service
         public static Board SolveBoard(Board board)
         {
             List<Tile> scanTiles = new List<Tile>(board.Tiles)
-                .Where(t => t.Type == TileType.NUMBER)
+                .Where(t => t.Type == TileType.NUMBER && t.Value != 0)
                 .ToList();
 
             bool boardChanged = true;
@@ -59,7 +59,7 @@ namespace FlagsSolver.Service
 
             bool boardChanged = false;
 
-            if (remainingFlagCount == 0 && tile.Value != 0)
+            if (remainingFlagCount == 0)
             {
                 boardChanged = true;
                 adjacentTiles.ForEach(t => {
